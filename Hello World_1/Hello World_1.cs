54938728fd5fc30230ec5846b1aee799b0169960
jsflenindvd <148946636+jsflenindvd@users.noreply.github.com>
2023-10-25 15:15:55 +0400

    Add project files.

---
+++ Hello World_1/Hello World_1.cs
@@ -0,0 +1,25 @@
+namespace Hello_World_1
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Text;
+	using Skyline.DataMiner.Automation;
+	
+	/// <summary>
+	/// Represents a DataMiner Automation script.
+	/// </summary>
+	public class Script
+	{
+		/// <summary>
+		/// The script entry point.
+		/// </summary>
+		/// <param name="engine">Link with SLAutomation process.</param>
+		public void Run(IEngine engine)
+		{
+			engine.GenerateInformation("Hello World");
+			var element = engine.FindElement("LocalHost");
+			engine.GenerateInformation("IP: " + element.PollingIP);
+		}
+	}
+}
